f162c62b5ecf7a7f5dd0f9828437b1f3e32603b1
Phohou <danliu031505@gmail.com>
2025-03-22 09:36:20 -0500

    init commit

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='p-5 flex justify-between items-center border shadow-md'>
+            <h1 className='text-2xl font-bold'>Todo App</h1>
+        </div>
+    )
+}
+
+export default Header
